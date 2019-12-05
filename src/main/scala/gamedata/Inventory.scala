69daff9a1b38382d17ced927775c483c335a10f6
Robin Adams <robin.adams.78@gmail.com>
2019-12-06 00:45:35 +0100

    Document Inventory

--- src/main/scala/gamedata/Inventory.scala
+++ src/main/scala/gamedata/Inventory.scala
@@ -1,5 +1,6 @@
 package gamedata
 
+/** The PC's inventory */
 case class Inventory(items: Map[Slot, Item] = Map(),
                      wearing: Option[Slot] = None,
                      wielding: Option[Slot] = None)
