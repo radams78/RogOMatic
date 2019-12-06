09a34d311d9cb24000a987f91988c3eeef8bcc04
Robin Adams <robin.adams.78@gmail.com>
2019-12-06 09:58:50 +0100

    Document Weapon

--- src/main/scala/gamedata/Weapon.scala
+++ src/main/scala/gamedata/Weapon.scala
@@ -1,18 +1,22 @@
 package gamedata
 
+/** A weapon in the game of Rogue */
 trait Weapon extends Item
 
+/** A melee weapon or bow */
 case class Wieldable(weaponType: WieldableType, plusToHit: Int, plusDamage: Int) extends Weapon {
   override def toString: String =
     s"${if (plusToHit > 0) "+" else ""}$plusToHit,${if (plusDamage > 0) "+" else ""}$plusDamage $weaponType"
 }
 
+/** A stack of missiles */
 case class Missile(quantity: Int, weaponType: MissileType, plusToHit: Int, plusDamage: Int) extends Weapon {
   override def toString: String =
     s"$quantity ${if (plusToHit > 0) "+" else ""}$plusToHit,${if (plusDamage > 0) "+" else ""}$plusDamage " +
       s"${if (quantity > 0) weaponType.singular else weaponType.plural}"
 }
 
+/** Factory methods for [[Weapon]] */
 object Weapon {
   def apply(weaponType: WeaponType, plusToHit: Int, plusDamage: Int): Weapon = weaponType match {
     case wt: WieldableType => Wieldable(wt, plusToHit, plusDamage)
