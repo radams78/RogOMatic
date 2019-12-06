6f0b4980f5e27e53d09572cb0cbc0e732837af3b
Robin Adams <robin.adams.78@gmail.com>
2019-12-06 10:01:13 +0100

    Print bonuses correctly

--- src/main/scala/gamedata/Weapon.scala
+++ src/main/scala/gamedata/Weapon.scala
@@ -6,13 +6,13 @@ trait Weapon extends Item
 /** A melee weapon or bow */
 case class Wieldable(weaponType: WieldableType, plusToHit: Int, plusDamage: Int) extends Weapon {
   override def toString: String =
-    s"${if (plusToHit > 0) "+" else ""}$plusToHit,${if (plusDamage > 0) "+" else ""}$plusDamage $weaponType"
+    s"${if (plusToHit >= 0) "+" else ""}$plusToHit,${if (plusDamage >= 0) "+" else ""}$plusDamage $weaponType"
 }
 
 /** A stack of missiles */
 case class Missile(quantity: Int, weaponType: MissileType, plusToHit: Int, plusDamage: Int) extends Weapon {
   override def toString: String =
-    s"$quantity ${if (plusToHit > 0) "+" else ""}$plusToHit,${if (plusDamage > 0) "+" else ""}$plusDamage " +
+    s"$quantity ${if (plusToHit >= 0) "+" else ""}$plusToHit,${if (plusDamage >= 0) "+" else ""}$plusDamage " + // TODO Extract method to print bonus
       s"${if (quantity > 0) weaponType.singular else weaponType.plural}"
 }
 
