8bed59877da65316d8198d170402d61f9ddb007f
Robin Adams <robin.adams.78@gmail.com>
2019-12-07 19:32:35 +0100

    Document IRogue

--- src/main/scala/rogue/IRogue.scala
+++ src/main/scala/rogue/IRogue.scala
@@ -5,7 +5,9 @@ trait IRogue {
   /** Start the game of Rogue */
   def start(): Unit
 
+  /** The current screen being displayed by Rogue */
   def getScreen: String
 
+  /** Send a character to Rogue as if from the user */
   def sendKeypress(keyPress: Char): Unit
 }
