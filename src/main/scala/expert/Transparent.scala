d97b73e1c03bf8a29930c1c85a55b5ae18dc9b3d
Robin Adams <robin.adams.78@gmail.com>
2019-12-07 19:45:02 +0100

    Extract Transparent

---
+++ src/main/scala/expert/Transparent.scala
@@ -0,0 +1,29 @@
+package expert
+
+import rogue.{Command, RoguePlayer}
+import view.IView
+
+import scala.io.StdIn
+
+/** Expert for playing the game in transparent mode, i.e. interactively, getting moves from the user one by one */
+class Transparent(player: RoguePlayer, view: IView) {
+  /** Play a game of rogue */
+  def playRogue(): Unit = {
+    player.start()
+    while (!player.gameOver) {
+      view.displayScreen(player.getScreen)
+      view.displayInventory(player.getInventory)
+      var cmd: Char = StdIn.readChar()
+      cmd match {
+        case 'h' => player.sendCommand(Command.LEFT)
+        case 'j' => player.sendCommand(Command.UP)
+        case 'k' => player.sendCommand(Command.DOWN)
+        case 'l' => player.sendCommand(Command.RIGHT)
+        case 'n' => player.sendCommand(Command.DOWNRIGHT)
+        case 'u' => player.sendCommand(Command.UPRIGHT)
+        case 'y' => player.sendCommand(Command.UPLEFT)
+        case '.' => player.sendCommand(Command.REST)
+      }
+    }
+  }
+}
--- src/main/scala/rogomatic/RogOMatic.scala
+++ src/main/scala/rogomatic/RogOMatic.scala
@@ -1,31 +1,15 @@
 package rogomatic
 
-import rogue.{Command, Rogue, RoguePlayer}
+import expert.Transparent
+import rogue.{Rogue, RoguePlayer}
 import view.TextView
-
-import scala.io.StdIn
 
 /** Entry point for the system */
 object RogOMatic extends App {
   val rogue = new Rogue
   val view = new TextView
   val player = new RoguePlayer(rogue)
-  player.start()
-  while (!player.gameOver) {
-    view.displayScreen(player.getScreen)
-    view.displayInventory(player.getInventory)
-    var cmd: Char = StdIn.readChar()
-    cmd match {
-      case 'h' => player.sendCommand(Command.LEFT)
-      case 'j' => player.sendCommand(Command.UP)
-      case 'k' => player.sendCommand(Command.DOWN)
-      case 'l' => player.sendCommand(Command.RIGHT)
-      case 'n' => player.sendCommand(Command.DOWNRIGHT)
-      case 'u' => player.sendCommand(Command.UPRIGHT)
-      case 'y' => player.sendCommand(Command.UPLEFT)
-      case '.' => player.sendCommand(Command.REST)
-    }
-  }
+  new Transparent(player, view)
   rogue.close()
   System.exit(0)
 }
