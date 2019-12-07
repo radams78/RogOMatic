72291a984fff78320d16cbfc8cd1234e31ce92bb
Robin Adams <robin.adams.78@gmail.com>
2019-12-07 19:31:51 +0100

    Document RoguePlayer

--- src/main/scala/rogue/RoguePlayer.scala
+++ src/main/scala/rogue/RoguePlayer.scala
@@ -2,11 +2,15 @@ package rogue
 
 import gamedata.Inventory
 
+/** High-level communication with the game of Rogue */
 class RoguePlayer(rogue: IRogue) {
+  /** Send a command to Rogue */
   def sendCommand(command: Command): Unit = rogue.sendKeypress(command.keypress)
 
+  /** True if the game is over */
   def gameOver: Boolean = false
 
+  /** Current inventory */
   def getInventory: Inventory = {
     rogue.sendKeypress('i')
     val screen: String = rogue.getScreen
@@ -14,8 +18,11 @@ class RoguePlayer(rogue: IRogue) {
     Inventory.parseInventoryScreen(screen).getOrElse(throw new Error(s"Could not parse inventory screen: $screen")) // TODO Better error handling?
   }
 
+  /** Current screen being displayed by Rogue */
   def getScreen: String = rogue.getScreen
 
+  /** Start the game */
+  // TODO What to do if game is already started?
   def start(): Unit = rogue.start()
 
 }
