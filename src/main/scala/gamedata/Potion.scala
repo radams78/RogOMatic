c139cee421ae7790210c7894b299386df8928a4e
Robin Adams <robin.adams.78@gmail.com>
2019-12-10 22:26:05 +0100

    String printing of potions

--- src/main/scala/gamedata/Potion.scala
+++ src/main/scala/gamedata/Potion.scala
@@ -1,4 +1,6 @@
 package gamedata
 
 /** A stack of potions */
-case class Potion(quantity: Int, colour: Colour) extends Item
+case class Potion(quantity: Int, colour: Colour) extends Item {
+  override def toString: String = s"$quantity $colour potion" + (if (quantity > 1) "s" else "")
+}
