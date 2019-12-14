eb82361af88292f5dd3e519399b5a643e3a57405
Robin Adams <robin.adams.78@gmail.com>
2019-12-14 12:09:08 +0100

    Document Armor

--- src/main/scala/gamedata/Armor.scala
+++ src/main/scala/gamedata/Armor.scala
@@ -1,15 +1,19 @@
 package gamedata
 
+/** A suit of armor */
 trait Armor extends Item
 
+/** An identified suit of armor */
 case class IdentifiedArmor(armorType: ArmorType, bonus: Bonus) extends Armor {
   override def toString: String = s"$bonus $armorType"
 }
 
+/** An unidentified suit of armor */
 case class UnidentifiedArmor(armorType: ArmorType) extends Armor {
   override def toString: String = armorType.toString
 }
 
+/** Factory methods for [[Armor]] */
 object Armor {
   def apply(armorType: ArmorType, bonus: Bonus): Armor = IdentifiedArmor(armorType, bonus)
 
