e2e933fa709667e8058ee06f289788176183b5ec
Robin Adams <robin.adams.78@gmail.com>
2019-12-15 22:00:56 +0100

    Add toString to Scroll

--- src/main/scala/gamedata/Scroll.scala
+++ src/main/scala/gamedata/Scroll.scala
@@ -1,4 +1,6 @@
 package gamedata
 
 /** A stack of scrolls */
-case class Scroll(quantity: Int, title: String) extends Item
+case class Scroll(quantity: Int, title: String) extends Item {
+  override def toString: String = s"a scroll entitled: '$title'"
+}
