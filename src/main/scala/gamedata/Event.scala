169adae6516c4b5fbd97bc17d233c5e157e0adec
Robin Adams <robin.adams.78@gmail.com>
2019-12-28 22:29:57 +0100

    Document Event

--- src/main/scala/gamedata/Event.scala
+++ src/main/scala/gamedata/Event.scala
@@ -2,12 +2,15 @@ package gamedata
 
 import rogue.Command
 
+/** An event indicated by a message in the message line */
 object Event extends Enumeration {
   type Event = Value
 
+  /** PC quaffed a potion of healing */
   val HEALING: Event = Val("you begin to feel better",
     GameState(Command.Quaff(Potion(PotionPower.HEALING))))
 
+  /** PC read a scroll of remove curse */
   val REMOVE_CURSE: Event = Val("you feel as though someone is watching over you",
     GameState(Command.Read(Scroll(ScrollPower.REMOVE_CURSE))))
 
