c654c3eb1118ed2242e8229ed20a5aaf55bd45cd
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 23:30:40 +0100

    Add full list of scroll powers

--- src/main/scala/gamedata/ScrollPower.scala
+++ src/main/scala/gamedata/ScrollPower.scala
@@ -9,7 +9,19 @@ object ScrollPower extends ParsableEnum {
 
   implicit def valueToScrollPowertVal(x: Value): Val = x.asInstanceOf[Val]
 
-  val REMOVE_CURSE: ScrollPower = Val("you feel as though someone is watching over you")
+  val AGGRAVATE_MONSTER: ScrollPower = Val("aggravate monster")
+  val CREATE_MONSTER: ScrollPower = Val("create monster")
+  val CONFUSE_MONSTER: ScrollPower = Val("confuse monster")
+  val ENCHANT_ARMOR: ScrollPower = Val("enchant armor")
+  val ENCHANT_WEAPON: ScrollPower = Val("enchant weapon")
+  val HOLD_MONSTER: ScrollPower = Val("hold monster")
+  val IDENTIFY: ScrollPower = Val("identify")
+  val MAGIC_MAPPING: ScrollPower = Val("magic mapping")
+  val PROTECT_ARMOR: ScrollPower = Val("protect armor")
+  val REMOVE_CURSE: ScrollPower = Val("remove curse")
+  val SCARE_MONSTER: ScrollPower = Val("scare monster")
+  val SLEEP: ScrollPower = Val("sleep")
+  val TELEPORTATION: ScrollPower = Val("teleportation")
 
   protected case class Val(effect: String) extends super.Val
 
