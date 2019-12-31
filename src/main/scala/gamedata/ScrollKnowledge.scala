63b1c65f8180b58e4f2f5f4aea23f5cfc55a94c0
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 23:02:05 +0100

    Document ScrollKnowledge

--- src/main/scala/gamedata/ScrollKnowledge.scala
+++ src/main/scala/gamedata/ScrollKnowledge.scala
@@ -3,9 +3,12 @@ package gamedata
 import gamedata.Domain._
 import gamedata.ScrollPower.ScrollPower
 
+/** Memory of which scroll titles map to which powers */
 case class ScrollKnowledge(private val powers: Map[String, ScrollPower]) {
+  /** Given a power, return the corresponding title, if known */
   def getTitle(p: ScrollPower): Option[String] = powers.find(_._2 == p).map(_._1)
 
+  /** Given a title, return the corresponding power, if known */
   def getPower(title: String): Option[ScrollPower] = powers.get(title)
 }
 
