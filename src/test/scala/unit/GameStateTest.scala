24e8a85f40bc8bedf8f955dcea77da126a602ca8
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 06:37:36 +0100

    Tidy up GameStateTest

--- src/test/scala/unit/GameStateTest.scala
+++ src/test/scala/unit/GameStateTest.scala
@@ -1,16 +1,17 @@
 package unit
 
 import gamedata._
+import mock.EitherAssertion._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import rogue.Command
 
 class GameStateTest extends AnyFlatSpec with Matchers {
   "Quaffing a potion of healing" should "record the power of healing" in {
-    Event.interpretMessage("you begin to feel better").flatMap((gs: GameState) =>
-      GameState.build(Command.Quaff(Some(Slot.A), Potion(1, Colour.RED))).flatMap(_.merge(gs))) match {
-      case Right(gs) => gs.potionKnowledge.getPower(Colour.RED) should contain(PotionPower.HEALING)
-      case Left(s) => fail(s)
-    }
+    getEither(for {
+      gs <- GameState.build(Command.Quaff(Some(Slot.A), Potion(1, Colour.RED)))
+      gs2 <- Event.interpretMessage("you begin to feel better")
+      gs3 <- gs.merge(gs2)
+    } yield gs3).potionKnowledge.getPower(Colour.RED) should contain(PotionPower.HEALING)
   }
 }
