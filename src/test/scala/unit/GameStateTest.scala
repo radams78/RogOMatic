a27ca883efce58376041d0af20f1e925693c2cc7
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 20:41:56 +0100

    Better variable name

--- src/test/scala/unit/GameStateTest.scala
+++ src/test/scala/unit/GameStateTest.scala
@@ -10,7 +10,7 @@ class GameStateTest extends AnyFlatSpec with Matchers {
   "Quaffing a potion of healing" should "record the power of healing" in {
     getEither(for {
       e <- Event.interpretMessage("you begin to feel better")
-      gs3 <- GameState(Command.Quaff(Some(Slot.A), Potion(1, Colour.RED))).merge(e.inference)
-    } yield gs3).potionKnowledge.getPower(Colour.RED) should contain(PotionPower.HEALING)
+      gs <- GameState(Command.Quaff(Some(Slot.A), Potion(1, Colour.RED))).merge(e.inference)
+    } yield gs).potionKnowledge.getPower(Colour.RED) should contain(PotionPower.HEALING)
   }
 }
