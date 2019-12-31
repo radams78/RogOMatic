a00d64a7e363704b331e4619a69accc7fe4e0ace
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 22:39:55 +0100

    Document invariants

--- src/main/scala/gamedata/Scroll.scala
+++ src/main/scala/gamedata/Scroll.scala
@@ -18,12 +18,7 @@ case class Scroll(quantity: Option[Int], title: Option[String], power: Option[Sc
       (for (p <- power) yield s"of $p") ++
       (for (t <- title) yield s"entitled: '$t'")).mkString(" ")
 
-  /** Infer what we can about this scroll from the fact that title is mapped to power */
-  def infer(_title: String, _power: ScrollPower): Either[String, Scroll] = for {
-    inferredTitle <- if (power contains _power) title.merge(Some(_title)) else Right(title)
-    inferredPower <- if (title contains _title) power.merge(Some(_power)) else Right(power)
-  } yield Scroll(quantity, inferredTitle, inferredPower)
-
+  /** Infer what we can about this scroll from the given information */
   def infer(scrollKnowledge: ScrollKnowledge): Either[String, Scroll] = (title, power) match {
     case (Some(t), power) => for {p <- power.merge(scrollKnowledge.getPower(t))} yield Scroll(quantity, title, p)
     case (None, Some(p)) => Right(Scroll(quantity, scrollKnowledge.getTitle(p), Some(p)))
@@ -45,7 +40,5 @@ object Scroll {
 
   def apply(quantity: Int, title: String): Scroll = Scroll(Some(quantity), Some(title), None)
 
-  implicit def scrollDomain: Domain[Scroll] = (x: Scroll, y: Scroll) => for {quantity <- x.quantity.merge(y.quantity)
-                                                                             title <- x.title.merge(y.title)
-                                                                             power <- x.power.merge(y.power)} yield Scroll(quantity, title, power)
+  implicit def scrollDomain: Domain[Scroll] = (x: Scroll, y: Scroll) => x.merge(y)
 }
