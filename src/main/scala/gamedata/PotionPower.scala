83cc44be8398707b300acf82e14ac9d48cc8b94b
Robin Adams <robin.adams.78@gmail.com>
2019-12-31 23:34:06 +0100

    Add full list of potion powers

--- src/main/scala/gamedata/PotionPower.scala
+++ src/main/scala/gamedata/PotionPower.scala
@@ -8,7 +8,20 @@ object PotionPower extends ParsableEnum {
 
   implicit def valueToPotionPowertVal(x: Value): Val = x.asInstanceOf[Val]
 
-  val HEALING: PotionPower = Val("you begin to feel better")
+  val BLINDNESS: PotionPower = Val("blindness")
+  val CONFUSION: PotionPower = Val("confusion")
+  val DETECT_MONSTER: PotionPower = Val("detect monster")
+  val DETECT_THINGS: PotionPower = Val("detect things")
+  val EXTRA_HEALING: PotionPower = Val("extra healing")
+  val HALLUCINATION: PotionPower = Val("hallucination")
+  val HASTE_SELF: PotionPower = Val("haste self")
+  val HEALING: PotionPower = Val("healing")
+  val INCREASE_STRENGTH: PotionPower = Val("increase strength")
+  val LEVITATION: PotionPower = Val("levitation")
+  val POISON: PotionPower = Val("poison")
+  val RAISE_LEVEL: PotionPower = Val("raise level")
+  val RESTORE_STRENGTH: PotionPower = Val("restore strength")
+  val SEE_INVISIBLE: PotionPower = Val("see invisible")
 
   protected case class Val(effect: String) extends super.Val
 
