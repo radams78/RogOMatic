c89db7334520d59c4536e8fad073a0f5b770eb3a
Robin Adams <robin.adams.78@gmail.com>
2020-01-10 23:39:05 +0100

    Extract UsesKnowledge typeclass

--- src/main/scala/gamedata/ProvidesKnowledge.scala
+++ src/main/scala/gamedata/ProvidesKnowledge.scala
@@ -7,3 +7,22 @@ trait ProvidesKnowledge { // TODO Not compositional
 
   def potionKnowledge: Either[String, PotionKnowledge] = Right(PotionKnowledge()) // TODO Demand Right?
 }
+
+trait UsesKnowledge[T] {
+  def infer(_this: T, scrollKnowledge: ScrollKnowledge): Either[String, T]
+
+  def infer(_this: T, potionKnowledge: PotionKnowledge): Either[String, T]
+}
+
+object UsesKnowledge {
+
+  implicit final class UsesKnowledgeOps[T](self: T)(implicit s: UsesKnowledge[T]) {
+    def infer(item: ProvidesKnowledge): Either[String, T] = for {
+      sk <- item.scrollKnowledge
+      item2 <- s.infer(self, sk)
+      pk <- item.potionKnowledge
+      item3 <- s.infer(item2, pk)
+    } yield item3
+  }
+
+}
