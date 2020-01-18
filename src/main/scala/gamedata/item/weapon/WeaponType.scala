16c20ca757e1479d2cc13a3de27ed9ed040c5ade
Robin Adams <robin.adams.78@gmail.com>
2020-01-18 21:17:41 +0100

    Create values for each weapon type in WeaponType

--- src/main/scala/gamedata/item/weapon/WeaponType.scala
+++ src/main/scala/gamedata/item/weapon/WeaponType.scala
@@ -6,12 +6,6 @@ import gamedata.item.weapon.Missiletype.MissileType
 /** An enum for the set of weapon types in the game of Rogue */
 trait WeaponType
 
-
-
-
-
-
-
 object WeaponType {
   val SHORT_BOW: WeaponType = ShooterType.SHORT_BOW
 
@@ -19,17 +13,27 @@ object WeaponType {
 
   val MACE: MeleeType = MeleeType.MACE
 
+  val DART: WeaponType = Missiletype.DART
+
+  val DAGGER: WeaponType = Missiletype.DAGGER
+
+  val SHURIKEN: WeaponType = Missiletype.SHURIKEN
+
+  val LONG_SWORD: WeaponType = MeleeType.LONG_SWORD
+
+  val TWO_HANDED_SWORD: WeaponType = MeleeType.TWO_HANDED_SWORD
+
   /** Given the name of a weapon, return the appropriate [[WeaponType]], or an error message if weapon type could not
    * be recognised */
   def parse(description: String): Either[String, WeaponType] = description match {
-    case "short bow" => Right(ShooterType.SHORT_BOW)
-    case "dart" | "darts" => Right(Missiletype.DART)
-    case "arrow" | "arrows" => Right(Missiletype.ARROW)
-    case "dagger" | "daggers" => Right(Missiletype.DAGGER)
-    case "shuriken" | "shurikens" => Right(Missiletype.SHURIKEN)
-    case "mace" => Right(MeleeType.MACE)
-    case "long sword" => Right(MeleeType.LONG_SWORD)
-    case "two-handed sword" => Right(MeleeType.TWO_HANDED_SWORD)
+    case "short bow" => Right(SHORT_BOW)
+    case "dart" | "darts" => Right(DART)
+    case "arrow" | "arrows" => Right(ARROW)
+    case "dagger" | "daggers" => Right(DAGGER)
+    case "shuriken" | "shurikens" => Right(SHURIKEN)
+    case "mace" => Right(MACE)
+    case "long sword" => Right(LONG_SWORD)
+    case "two-handed sword" => Right(TWO_HANDED_SWORD)
     case _ => Left(s"Unrecognised weapon type: $description")
   }
 }
