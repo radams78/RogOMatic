6c1a1e04a7c4384b67e929b69e8f9169059bdc91
Robin Adams <robin.adams.78@gmail.com>
2020-01-20 08:48:48 +0100

    Document IRecorder

--- src/main/scala/gamestate/IRecorder.scala
+++ src/main/scala/gamestate/IRecorder.scala
@@ -4,9 +4,12 @@ import expert.pGameState
 
 /** Interface for recorder that keeps track of current [[pGameState]] */
 trait IRecorder {
+  /** The current state of the game */
   def gameState: pGameState
 
+  /** The final score after the game has ended */
   def getScore: Int
 
+  /** True if the game has ended */
   def gameOver: Boolean
 }
