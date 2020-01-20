cf4e229530388fca80dabcbbf531492cebf78f3f
Robin Adams <robin.adams.78@gmail.com>
2020-01-21 00:06:58 +0100

    Move IRogueActuator to its own file

---
+++ src/main/scala/rogue/IRogueActuator.scala
@@ -0,0 +1,13 @@
+package rogue
+
+/** Interface for the actuator that performs high-level communication with the game of Rogue. */
+trait IRogueActuator {
+  /** Send a command to Rogue, then read all possible information from the screen and inventory. Returns Right(())
+   * if all information successfully read, Left(error message) if we read information incompatible with information
+   * already known. */
+  def sendCommand(getCommand: Command): Either[String, Unit]
+
+  /** Start the game of Rogue and read the first screen and inventory. Returns Right(()) if all information successfully
+   * read, Left(error message) if we read information incompatible with information already known. */
+  def start(): Either[String, Unit]
+}
--- src/main/scala/rogue/RogueActuator.scala
+++ src/main/scala/rogue/RogueActuator.scala
@@ -6,17 +6,7 @@ import gamestate.IInputRecorder
 import scala.annotation.tailrec
 import scala.util.matching.Regex
 
-/** Interface for the actuator that performs high-level communication with the game of Rogue. */
-trait IRogueActuator {
-  /** Send a command to Rogue, then read all possible information from the screen and inventory. Returns Right(())
-   * if all information successfully read, Left(error message) if we read information incompatible with information
-   * already known. */
-  def sendCommand(getCommand: Command): Either[String, Unit]
 
-  /** Start the game of Rogue and read the first screen and inventory. Returns Right(()) if all information successfully
-   * read, Left(error message) if we read information incompatible with information already known. */
-  def start(): Either[String, Unit]
-}
 
 /** High-level communication with the game of Rogue. */
 class RogueActuator(rogue: IRogue, recorder: IInputRecorder) extends IRogueActuator {
