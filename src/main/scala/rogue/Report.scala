4d4abe8e969fc53012b7d79796b419dde1660d8c
Robin Adams <robin.adams.78@gmail.com>
2020-01-21 19:29:03 +0100

    Document Report

--- src/main/scala/rogue/Report.scala
+++ src/main/scala/rogue/Report.scala
@@ -3,12 +3,17 @@ package rogue
 import gamedata.pInventory
 import rogue.Event.Event
 
+/** Report returned from parsing screens from Rogue */
 trait Report {
+  /** Final screen displayed by Rogue */
   def screen: String
 }
 
 object Report {
 
+  /** Game is still in progress
+   *
+   * @param events Set of events reported by message lines */
   case class GameOn(override val screen: String, inventory: pInventory, events: Set[Event]) extends Report
 
   case class GameOver(override val screen: String, score: Int) extends Report
