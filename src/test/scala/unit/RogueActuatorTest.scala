f07b8e794b505f025b984108948ccbed1de4ffb2
Robin Adams <robin.adams.78@gmail.com>
2020-01-21 23:07:53 +0100

    Inline fixtures

--- src/test/scala/unit/RogueActuatorTest.scala
+++ src/test/scala/unit/RogueActuatorTest.scala
@@ -10,37 +10,9 @@ import rogue._
 
 /** Unit tests for [[RogueActuator]] class */
 class RogueActuatorTest extends AnyFlatSpec with Matchers {
-
-  trait ZeroMoveGame {
+  it should "display the first screen of the game" in {
     val game: MockRogue = mock.ZeroMoveGame.zeroMoveGame
     val player: IRogueActuator = new RogueActuator(game)
-  }
-
-  trait EmptyInventoryGame {
-    val player: IRogueActuator = new RogueActuator(ZeroMoveGame.emptyInventoryGame)
-  }
-
-  trait TestGame {
-    val player: IRogueActuator = new RogueActuator(TestGame.testGame)
-  }
-
-  trait DeathGame {
-    val player: IRogueActuator = new RogueActuator(DeathGame.deathGame)
-  }
-
-  trait MoreGame {
-    val player: IRogueActuator = new RogueActuator(MoreGame.moreGame)
-  }
-
-  trait ReadScroll {
-    val player: IRogueActuator = new RogueActuator(MockRogue.Build
-      .WaitForCommand("readScroll state 1", TestGame.secondScreen, TestGame.secondInventoryScreen, 'r')
-      .Wait(TestGame.thirdScreen, 'f')
-      .Terminal("readScroll state 3", TestGame.fourthScreen, TestGame.fourthInventoryScreen)
-    )
-  }
-
-  it should "display the first screen of the game" in new ZeroMoveGame {
     player.start() match {
       case Right(report: Report.GameOn) => report.screen should be(ZeroMoveGame.firstScreen)
       case Right(report) => fail(s"Incorrect report type returned: $report")
@@ -48,7 +20,8 @@ class RogueActuatorTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "display the first inventory of the game" in new EmptyInventoryGame {
+  it should "display the first inventory of the game" in {
+    val player: IRogueActuator = new RogueActuator(ZeroMoveGame.emptyInventoryGame)
     player.start() match {
       case Right(report: Report.GameOn) => report.inventory should be(pInventory(Map[Slot, pItem](), None, None))
       case Right(report) => fail(s"Incorrect report type returned: $report")
@@ -56,33 +29,24 @@ class RogueActuatorTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "display the new screen after sending the command" in new TestGame {
+  it should "display the new screen after sending the command" in {
+    val player: IRogueActuator = new RogueActuator(TestGame.testGame)
     player.start() match {
       case Right(report: Report.GameOn) => ()
       case Right(report) => fail(s"Incorrect report type returned: $report")
       case Left(err) => fail(err)
     }
     player.sendCommand(Command.RIGHT) match {
-      case Right(report: Report.GameOn) => report.screen should be(TestGame.secondScreen)
+      case Right(report: Report.GameOn) =>
+        report.screen should be(TestGame.secondScreen)
+        report.inventory should be(TestGame.firstInventory)
       case Right(report) => fail(s"Incorrect report type returned: $report")
       case Left(err) => fail(err)
     }
   }
 
-  it should "display the inventory after sending the command" in new TestGame {
-    player.start() match {
-      case Right(report: Report.GameOn) => ()
-      case Right(report) => fail(s"Incorrect report type returned: $report")
-      case Left(err) => fail(err)
-    }
-    player.sendCommand(Command.RIGHT) match {
-      case Right(report: Report.GameOn) => report.inventory should be(TestGame.firstInventory)
-      case Right(report) => fail(s"Incorrect report type returned: $report")
-      case Left(err) => fail(err)
-    }
-  }
-
-  it should "know when the game is over" in new DeathGame {
+  it should "know when the game is over" in {
+    val player: IRogueActuator = new RogueActuator(DeathGame.deathGame)
     player.start() match {
       case Right(report: Report.GameOn) => ()
       case Right(report) => fail(s"Incorrect report type returned: $report")
@@ -95,7 +59,8 @@ class RogueActuatorTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "clear a more screen" in new MoreGame {
+  it should "clear a more screen" in {
+    val player: IRogueActuator = new RogueActuator(MoreGame.moreGame)
     player.sendCommand(Command.RIGHT) match {
       case Right(report: Report.GameOn) => report.screen should be(MoreGame.thirdScreen)
       case Right(report) => fail(s"Incorrect report type returned: $report")
@@ -103,7 +68,12 @@ class RogueActuatorTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  it should "remember scroll powers" in new ReadScroll {
+  it should "remember scroll powers" in {
+    val player: IRogueActuator = new RogueActuator(MockRogue.Build
+      .WaitForCommand("readScroll state 1", TestGame.secondScreen, TestGame.secondInventoryScreen, 'r')
+      .Wait(TestGame.thirdScreen, 'f')
+      .Terminal("readScroll state 3", TestGame.fourthScreen, TestGame.fourthInventoryScreen)
+    )
     player.sendCommand(Command.Read(Slot.F, Scroll(1, "coph rech"))) match {
       case Right(report: Report.GameOn) => report.events should contain(Event.REMOVE_CURSE)
       case Right(report) => fail(s"Incorrect report type returned: $report")
