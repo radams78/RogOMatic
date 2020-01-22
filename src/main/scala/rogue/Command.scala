a85b116ad1466629911ec5e216bffad221ebd969
Robin Adams <robin.adams.78@gmail.com>
2020-01-22 23:00:20 +0100

    Document Command

--- src/main/scala/rogue/Command.scala
+++ src/main/scala/rogue/Command.scala
@@ -2,27 +2,33 @@ package rogue
 
 import gamedata.{Slot, pCommand}
 
+/** A command that can be sent to [[IRogueActuator]] */
 trait Command {
+  /** _.topCommand is the embedding of Command in pCommand */
   def topCommand: pCommand
 
+  /** Keypresses to be sent to Rogue to perform this command */
   def keypresses: Seq[Char]
 
 }
 
 object Command {
 
+  /** Move right */
   object RIGHT extends Command {
     override def topCommand: pCommand = pCommand.RIGHT
 
     override def keypresses: Seq[Char] = Seq('l')
   }
 
+  /** Rest for one turn */
   object REST extends Command {
     override def topCommand: pCommand = pCommand.REST
 
     override def keypresses: Seq[Char] = Seq('.')
   }
 
+  /** Read a scroll */
   case class Read(slot: Slot) extends Command {
     override def topCommand: pCommand = pCommand.Read(slot)
 
