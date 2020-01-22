6ef5519be8f7c813b51fc073756ac80b9f4c5523
Robin Adams <robin.adams.78@gmail.com>
2020-01-22 22:37:59 +0100

    Delete History.gameState

--- src/main/scala/gamestate/History.scala
+++ src/main/scala/gamestate/History.scala
@@ -1,7 +1,6 @@
 package gamestate
 
 import domain.Domain._
-import domain.pLift
 import gamedata.ProvidesKnowledge._
 import gamedata.UsesKnowledge._
 import gamedata._
@@ -23,46 +22,48 @@ object History {
       case report: Report.GameOver => Right(GameOver(this, cmd, report))
     }
 
-    /** The current stae of the game that can be inferred from the history */
-    def gameState: pGameState
+    def knowledge: Set[Fact]
 
-    def knowledge: Set[Fact] = gameState.knowledge
-
-    def lastCommand: pLift[Option[pCommand]] = gameState.lastCommand
+    def lastCommand: Option[pCommand]
   }
 
   object GameOn {
-    implicit def providesKnowledge: ProvidesKnowledge[GameOn] = (self: GameOn) => self.inventory.implications union
-      self.knowledge union
-      (self.lastCommand match {
-        case pLift.UNKNOWN => Set()
-        case pLift.Known(None) => Set()
-        case pLift.Known(Some(command)) => command.implications
-      })
+    implicit def providesKnowledge: ProvidesKnowledge[GameOn] = (self: GameOn) => self.knowledge
   }
 
   /** The history of a game before the first move is made */
   case class FirstMove(report: Report.GameOn) extends GameOn {
-    override def gameState: pGameState = report.inferences
-
     override def screen: String = report.screen
 
     override def inventory: pInventory = report.inventory
+
+    override def knowledge: Set[Fact] = report.implications
+
+    override def lastCommand: Option[pCommand] = None
   }
 
   /** The history of a game with one or more moves that is not yet finished */
-  case class NextMove(history: GameOn, command: pCommand, report: Report.GameOn, override val gameState: pGameState, override val inventory: pInventory) extends GameOn {
+  case class NextMove(history: GameOn, command: pCommand, report: Report.GameOn, override val inventory: pInventory) extends GameOn {
     override def screen: String = report.screen
+
+    override def knowledge: Set[Fact] = history.knowledge.union(
+      report.implications.union(
+        inventory.implications.union(
+          command.implications
+        )
+      )
+    )
+
+    override def lastCommand: Option[pCommand] = Some(command)
   }
 
   object NextMove {
     def build(history: GameOn, command: pCommand, report: Report.GameOn): Either[String, NextMove] = for {
-      gs2 <- history.gameState.merge(pGameState(command))
-      gs4 <- gs2.merge(report.inferences)
       i2 <- history.inventory.infer(command)
       i3 <- i2.merge(report.inventory)
       i4 <- i3.infer(report) // TODO Ugly?
-    } yield NextMove(history, command, report, gs4, i4)
+
+    } yield NextMove(history, command, report, i4)
 
   }
 
