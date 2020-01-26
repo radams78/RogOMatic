18b01177161743234e63c0e9b58c4f0e6830faee
Robin Adams <robin.adams.78@gmail.com>
2020-01-26 23:26:27 +0100

    Add InSlot fact

--- src/test/scala/acceptance/TransparentGameSpec.scala
+++ src/test/scala/acceptance/TransparentGameSpec.scala
@@ -1,32 +1,16 @@
 package acceptance
 
 import mock.{MockRogue, MockUser, TestGame}
-import org.scalatest.GivenWhenThen
-import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import rogomatic.RogOMatic
 
-class TransparentGameSpec extends AnyFeatureSpec with GivenWhenThen with Matchers {
+class TransparentGameSpec extends AnyFunSuite with Matchers {
 
-  Feature("Play a game of Rogue in transparent mode") {
-    Scenario("User starts a game of Rogue in transparent mode") {
-      Given("an instance of Rog-O-Matic")
-      val rogue: MockRogue = TestGame.testGame
-      val user: MockUser = TestGame.user
-
-      When("the user starts the game in transparent mode")
-      RogOMatic.transparent(rogue, user).playRogue(user)
-
-      Then("the first screen should be displayed")
-      And("the first inventory should be displayed")
-      When("the user enters the command to go right")
-      Then("the second screen should be displayed")
-      And("the inventory should be displayed")
-      When("the user enters the command te read a scroll")
-      Then("the final screen should be displayed")
-      And("the final inventory should be displayed")
-      And("the scroll power should be remembered")
-      assert(user.finished)
-    }
+  test("Play a game of Rogue in transparent mode") {
+    val rogue: MockRogue = TestGame.testGame
+    val user: MockUser = TestGame.user
+    RogOMatic.transparent(rogue, user).playRogue(user)
+    assert(user.finished)
   }
 }
