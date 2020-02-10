d7c535c2e1ccdcebe69cabdbaf2f89f83a4bca74
Robin Adams <robin.adams.78@gmail.com>
2020-02-10 22:44:12 +0100

    Document Fact.after

--- src/main/scala/gamedata/fact/Fact.scala
+++ src/main/scala/gamedata/fact/Fact.scala
@@ -5,6 +5,8 @@ import gamedata.fact.UsesKnowledge._
 import gamedata.pCommand
 
 trait Fact {
+  /** If this is true before command is performed, then every member of this.after(command) is true after command
+   * is performed */
   final def after(command: pCommand): Either[String, Set[Fact]] =
     for (c <- command.infer(this)) yield c.implications
 }
