ce239ef5e4b32ee6ef98fa49fac028a8ae553406
Robin Adams <robin.adams.78@gmail.com>
2020-11-27 15:03:09 +0100

    Document RogOMatic

--- src/main/RogOMatic.scala
+++ src/main/RogOMatic.scala
@@ -3,8 +3,11 @@ package main
 import rogue.IRogue
 import view.IView
 
+/** The main class.
+ * 
+ * When the program starts, this connects a game of Rogue to a view. */
 class RogOMatic(rogue : IRogue, view : IView) {
-  val sensor: Sensor = new Sensor(rogue)
+  private val sensor: Sensor = new Sensor(rogue)
   sensor.subscribe(view)
 
   def startGame(): Unit = sensor.broadcastAll()
