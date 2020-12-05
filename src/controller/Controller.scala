88b4c869d3861fe929a8d0ce63ac834d37911b63
Robin Adams <robin.adams.78@gmail.com>
2020-12-05 23:32:02 +0100

    Document Controller

--- src/controller/Controller.scala
+++ src/controller/Controller.scala
@@ -3,7 +3,9 @@ package controller
 import gamedata.Command
 import model.IRoguePlayer
 
-/** A controller for Rog-O-Matic */
+/** A controller for Rog-O-Matic 
+ * 
+ * @param player The [[IRoguePlayer]] that commands are sent to */
 class Controller(player: IRoguePlayer) extends IController {
   override def performCommand(command: Command): Unit = ()
 }
