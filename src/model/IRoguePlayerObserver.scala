5f0a7579479afbd922ed63209ddd68db08f2bfbb
Robin Adams <robin.adams.78@gmail.com>
2020-12-05 23:37:37 +0100

    Document IRoguePlayerObserver

--- src/model/IRoguePlayerObserver.scala
+++ src/model/IRoguePlayerObserver.scala
@@ -2,5 +2,6 @@ package model
 
 /** An object that can observe an [[IRoguePlayer]] */
 trait IRoguePlayerObserver {
+  /** Notify all observers that this is the screen displayed by Rogue */
   def notify(screen: Seq[String]): Unit
 }
