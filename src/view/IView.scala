aba7a514259d67b1e34d97c91aff002ba036934d
Robin Adams <robin.adams.78@gmail.com>
2020-12-05 23:39:04 +0100

    Document IView

--- src/view/IView.scala
+++ src/view/IView.scala
@@ -2,6 +2,7 @@ package view
 
 import model.IRoguePlayerObserver
 
+/** A view that displays info to the user */
 trait IView extends IRoguePlayerObserver {
   def notify(screen: Seq[String]): Unit
 }
