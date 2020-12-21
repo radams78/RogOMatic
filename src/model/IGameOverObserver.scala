d6aa55bc2dacb6b2d614579aa2ffcfdf208983b8
radams78 <zoX.GJ^tHL}cfx=&>
2020-12-21 11:52:03 +0100

    Document IGameOverObserver

--- src/model/IGameOverObserver.scala
+++ src/model/IGameOverObserver.scala
@@ -1,6 +1,10 @@
 package model
 
+/** An interface for notifying observers that the game is over */
 trait IGameOverObserver {
 
+  /** Notify the observer that the game is over
+   *
+   * @param score The final score */
   def notifyGameOver(score: Int): Unit
 }
