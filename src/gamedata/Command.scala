6705c0b884dc383394377f8dde0b881ab3091ea5
radams78 <zoX.GJ^tHL}cfx=&>
2020-12-21 16:01:43 +0100

    Document Command

--- src/gamedata/Command.scala
+++ src/gamedata/Command.scala
@@ -2,7 +2,11 @@ package gamedata
 
 import rogue.IRogue
 
+/** A command in the game of Rogue */
 trait Command {
+  /** Send the command to the given Rogue process
+   *
+   * @param rogue The Rogue process to send the command to */
   def perform(rogue: IRogue): Unit
 }
 
@@ -11,7 +15,9 @@ object Command {
     override def perform(rogue: IRogue): Unit = keypresses.foreach(rogue.sendKeypress)
   }
 
+  /** Command to quit the game */
   val QUIT : Command = new KeypressCommand(Seq('Q', 'y', ' '))
 
+  /** Command to move left */
   val LEFT : Command = new KeypressCommand(Seq('h'))
 }
