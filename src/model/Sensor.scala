8d5efb4f04c143a309355a286ab4a5c2e8ec25d8
radams78 <zoX.GJ^tHL}cfx=&>
2020-12-24 23:07:40 +0100

    Rename observers to gameOverObservers

--- src/model/Sensor.scala
+++ src/model/Sensor.scala
@@ -6,13 +6,13 @@ import scala.util.matching.Regex
 
 /** Parse the information from the screen of Rogue and notify the relevant observers */
 class Sensor extends IScreenObserver {
-  private var observers: Seq[IGameOverObserver] = Seq()
+  private var gameOverObservers: Seq[IGameOverObserver] = Seq()
   private var _score : Option[Int] = None
 
   private val scoreLine: Regex = """with (?<score>\d+) gold""".r
 
   /** Add an observer that listens for the message that the game is over */
-  def addGameOverObserver(observer: IGameOverObserver): Unit = observers :+= observer
+  def addGameOverObserver(observer: IGameOverObserver): Unit = gameOverObservers :+= observer
 
   override def notify(screen: Screen): Unit = parseScreen(screen)
 
@@ -32,7 +32,7 @@ class Sensor extends IScreenObserver {
   }
 
   private def parseGameOverScreen(screen: Screen): Unit = {
-    for (observer <- observers)
+    for (observer <- gameOverObservers)
       observer.notifyGameOver(_score.getOrElse(throw new BadGameOverException("Game over screen received without final score")))
   }
 
