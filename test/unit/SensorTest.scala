ba8a815b47cdb1c4ce86c0c2cc579acc9e3cfd5b
radams78 <zoX.GJ^tHL}cfx=&>
2020-12-25 21:10:41 +0100

    Simplify test

--- test/unit/SensorTest.scala
+++ test/unit/SensorTest.scala
@@ -9,35 +9,6 @@ class SensorTest extends AnyFlatSpec with Matchers {
   "A sensor" should "recognize a game over screen" in {
     val sensor: Sensor = new Sensor
 
-    val screen4 =
-      """quit with 20 gold-more-
-        |                                                      --------------------
-        |                                                      +..%......@..*S....|
-        |                                                      |............./....|
-        |                                                      -------+------------
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |Level: 1  Gold: 20      Hp: 12(12)   Str: 16(16) Arm: 4  Exp: 1/0
-        |"""
-
-    val screen4lines = Screen.makeScreen(screen4)
-
     val screen5 =
       """-more-
         |
@@ -78,7 +49,6 @@ class SensorTest extends AnyFlatSpec with Matchers {
     }
 
     sensor.addGameOverObserver(MockObserver)
-    sensor.notify(screen4lines)
     sensor.notify(screen5lines)
     MockObserver should be(Symbol("seenGameOverScreen"))
   }
