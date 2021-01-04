b067a9f6ae1e663d28b70401d9ffc10a535408f2
Robin Adams <robin.adams.78@gmail.com>
2021-01-04 16:51:49 +0100

    Document RoguePlayer

--- src/model/RoguePlayer.scala
+++ src/model/RoguePlayer.scala
@@ -2,17 +2,34 @@ package model
 
 import model.rogue.IRogue
 
+/** Communicate with the game of Rogue */
 class RoguePlayer(rogue : IRogue) {
+  /** Send the given command to Rogue.
+   * 
+   * The game of Rogue must be in progress, otherwise throws an [[GameNotInProgressException]]
+   * 
+   * @param command Command to be performed */
   def performCommand(command: Command): Unit = ()
 
+  /** Start the game of Rogue 
+   * 
+   * If the game of Rogue has already started, throws an [[GameStartedException]]*/
   def startGame(): Unit = rogue.startGame()
 
+  /** Add an observer that listens for the final score */
   def addScoreObserver(observer: IScoreObserver): Unit = ()
 
+  /** Add an observer that listens for the message that the game is over */
   def addGameOverObserver(observer: IGameOverObserver): Unit = ()
 
+  /** Add an observer that listens for the current inventory */
   def addInventoryObserver(observer: IInventoryObserver): Unit = ()
 
+  /** Add an observer that listens for the screen retrieved from Rogue */
   def addScreenObserver(observer: IScreenObserver): Unit = ()
 
 }
+
+class GameNotInProgressException extends Exception
+
+class GameStartedException extends Exception
