cdd4bd84925c7ccb9f2f6ecb1493db837d30ebe0
Robin Adams <robin.adams.78@gmail.com>
2021-01-05 21:25:21 +0100

    Extract fixture

--- src/model/RoguePlayer.scala
+++ src/model/RoguePlayer.scala
@@ -1,9 +1,11 @@
 package model
 
-import model.rogue.IRogue
+import model.rogue.{IRogue, Screen}
 
 /** Communicate with the game of Rogue */
 class RoguePlayer(rogue : IRogue) {
+  private var screenObservers : Set[IScreenObserver] = Set()
+  
   /** Send the given command to Rogue.
    * 
    * The game of Rogue must be in progress, otherwise throws an [[GameNotInProgressException]]
@@ -14,7 +16,11 @@ class RoguePlayer(rogue : IRogue) {
   /** Start the game of Rogue 
    * 
    * If the game of Rogue has already started, throws an [[GameStartedException]]*/
-  def startGame(): Unit = rogue.startGame()
+  def startGame(): Unit = {
+    rogue.startGame()
+    val screen : Screen = rogue.getScreen.getOrElse(throw new EmptyScreenException)
+    for (observer <- screenObservers) observer.notify(screen)
+  }
 
   /** Add an observer that listens for the final score */
   def addScoreObserver(observer: IScoreObserver): Unit = ()
@@ -26,10 +32,12 @@ class RoguePlayer(rogue : IRogue) {
   def addInventoryObserver(observer: IInventoryObserver): Unit = ()
 
   /** Add an observer that listens for the screen retrieved from Rogue */
-  def addScreenObserver(observer: IScreenObserver): Unit = ()
+  def addScreenObserver(observer: IScreenObserver): Unit = screenObservers = screenObservers + observer
 
 }
 
 class GameNotInProgressException extends Exception
 
 class GameStartedException extends Exception
+
+class EmptyScreenException extends Exception
--- test/unit/RoguePlayerTest.scala
+++ test/unit/RoguePlayerTest.scala
@@ -1,12 +1,48 @@
 package unit
 
-import model.RoguePlayer
+import model.{IScreenObserver, RoguePlayer}
 import model.rogue.{IRogue, Screen}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class RoguePlayerTest extends AnyFlatSpec with Matchers {
+  def fixture: Object {
+    val screen: Screen
+  } = new {
+    val screen: Screen = Screen.makeScreen(
+      """
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |  -------+---
+        |  |.@.......|
+        |  |.........|
+        |  |.........|
+        |  |........*|
+        |  -----------
+        |Level: 1  Gold: 0      Hp: 12(12)   Str: 16(16) Arm: 4  Exp: 1/0
+        |""".stripMargin
+    )
+  }
+
   "A Rogue player" should "start the game of Rogue" in {
+    val f: Object {
+      val screen: Screen
+    } = fixture
+    
     object MockRogue extends IRogue {
       private var _started: Boolean = false
       
@@ -16,11 +52,45 @@ class RoguePlayerTest extends AnyFlatSpec with Matchers {
 
       override def startGame(): Unit = _started = true
 
-      override def getScreen: Option[Screen] = None
+      override def getScreen: Option[Screen] = if (_started) Some(f.screen) else None
     }
     
     val player : RoguePlayer = new RoguePlayer(MockRogue)
     player.startGame()
     MockRogue should be(Symbol("started"))
+  }
+  
+  "A Rogue player" should "broadcast the screen from Rogue" in {
+    val f: Object {
+      val screen: Screen
+    } = fixture
+    
+    object MockRogue extends IRogue {
+      private var _started: Boolean = false
+
+      def started: Boolean = _started
+
+      override def sendKeypress(keypress: Char): Unit = ()
+
+      override def startGame(): Unit = _started = true
+
+      override def getScreen: Option[Screen] = if (_started) Some(f.screen) else None
+    }
+
+    object MockObserver extends IScreenObserver {
+      private var _seenScreen: Boolean = false
+      
+      def seenScreen: Boolean = _seenScreen
+      
+      override def notify(_screen: Screen): Unit = {
+        _screen should be(f.screen)
+        _seenScreen = true
+      }
+    }
+    
+    val player : RoguePlayer = new RoguePlayer(MockRogue)
+    player.addScreenObserver(MockObserver)
+    player.startGame()
+    MockObserver should be(Symbol("seenScreen"))
   }
 }
