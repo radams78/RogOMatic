c36da4ac823b65845b3ce5d500fd9cf79c7fe181
Robin Adams <robin.adams.78@gmail.com>
2021-01-08 22:03:25 +0100

    Document ScreenReader

--- src/model/ScreenReader.scala
+++ src/model/ScreenReader.scala
@@ -1,7 +1,13 @@
 package model
 
-import model.rogue.Screen
+import model.rogue.{Screen, IRogue}
 
+/** Take the screen displayed by Rogue and make it available via a getter and the observer pattern.
+ * 
+ * An object of this class is injected when an [[IRogue]] object is created. After the game of Rogue starts,
+ * readScreen() returns the current screen displayed by the Rogue process, and all observers are notified whenever
+ * the screen changes. Before the game starts or after the game ends, readScreen will return None. */
+// TODO Return None after game ends
 class ScreenReader {
   private var _screen : Option[Screen] = None
   
