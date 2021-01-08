facaeb1a84e5bb274275e9176ef27e0b0f9adf5e
Robin Adams <robin.adams.78@gmail.com>
2021-01-08 21:57:05 +0100

    Document TransparentController

--- src/controller/TransparentController.scala
+++ src/controller/TransparentController.scala
@@ -2,6 +2,14 @@ package controller
 
 import model.{Command, RoguePlayer}
 
+/** A controller that allows the user to play the game of Rogue in transparent mode.
+ * 
+ * This takes commands input from StdIn and passes them on to the given [[RoguePlayer]]. If the user enters an invalid
+ * command, it prints an erro message and waits for input again.
+ * 
+ * This class is a humble object. */
+
+// TODO End thread when game is over
 class TransparentController(player: RoguePlayer) extends Runnable {
   override def run(): Unit = while (true) {
     val input: Char = scala.io.StdIn.readChar()
