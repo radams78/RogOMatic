116f5f5ba77c9ae38e7bfe7e7e4d7de6966f7b53
Robin Adams <robin.adams.78@gmail.com>
2021-01-17 17:03:46 +0100

    Document Rogue.DEFAULT_ENVIRONMENT

--- src/model/rogue/Rogue.scala
+++ src/model/rogue/Rogue.scala
@@ -2,13 +2,19 @@ package model.rogue
 
 import com.pty4j.PtyProcess
 
-  object Rogue {
-    private val DEFAULT_COMMAND: Array[String] = Array("/usr/games/rogue")
-    private val DEFAULT_ENVIRONMENT: java.util.Map[String, String] = new java.util.HashMap[String, String]
-    DEFAULT_ENVIRONMENT.put("TERM", "xterm")
+/** Start an instance of Rogue running.
+ * 
+ * This starts Rogue running in a separate process and wraps it in a [[RogueProcess]] object. This is a humble object. */
+object Rogue {
+  // Command to launch the Rogue process
+  private val DEFAULT_COMMAND: Array[String] = Array("/usr/games/rogue")
+  
+  // Environment for the Rogue process
+  private val DEFAULT_ENVIRONMENT: java.util.Map[String, String] = new java.util.HashMap[String, String]
+  DEFAULT_ENVIRONMENT.put("TERM", "xterm")
 
-    def apply(screenReader: ScreenReader): IRogue = {
-      val pty: PtyProcess = PtyProcess.exec(DEFAULT_COMMAND, DEFAULT_ENVIRONMENT)
-      new RogueProcess(screenReader, pty)
-    }
+  def apply(screenReader: ScreenReader): IRogue = {
+    val pty: PtyProcess = PtyProcess.exec(DEFAULT_COMMAND, DEFAULT_ENVIRONMENT)
+    new RogueProcess(screenReader, pty)
   }
+}
