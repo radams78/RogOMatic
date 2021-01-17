de72521fa699902c7da53f4d8ad6148e0dcaeb88
Robin Adams <robin.adams.78@gmail.com>
2021-01-17 09:02:52 +0100

    Create RogueProcess.charset

--- src/model/rogue/RogueProcess.scala
+++ src/model/rogue/RogueProcess.scala
@@ -1,5 +1,11 @@
 package model.rogue
 
-class RogueProcess {
+import java.nio.charset.Charset
 
+class RogueProcess {
+  val charset: Charset = RogueProcess.DEFAULT_CHARSET
+}
+
+object RogueProcess {
+  private val DEFAULT_CHARSET: Charset = Charset.forName("UTF-8")
 }
