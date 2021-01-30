45e71e382c09fb4343446fc93fb376a4fd9eecdd
Robin Adams <robin.adams.78@gmail.com>
2021-01-31 00:59:37 +0100

    Document IRoguePlayer

--- src/model/rogue/IRoguePlayer.scala
+++ src/model/rogue/IRoguePlayer.scala
@@ -1,16 +1,16 @@
 package model.rogue
 
+/** A facade for the [[model.rogue]] package */
 trait IRoguePlayer {
-  def addInventoryObserver(observer: IInventoryObserver)  : Unit
+  def startGame(): Unit
 
   def performCommand(command: Command): Unit
 
-  def startGame(): Unit
+  def addScreenObserver(observer: IScreenObserver)
+
+  def addInventoryObserver(observer: IInventoryObserver)  : Unit
 
   def addScoreObserver(observer: IScoreObserver)
 
   def addGameOverObserver(observer: IGameOverObserver)
-
-  def addScreenObserver(observer: IScreenObserver)
-
 }
