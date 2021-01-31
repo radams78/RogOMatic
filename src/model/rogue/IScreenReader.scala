af18bbc4466dedbe1d91c5d903561dea5c3b185e
Robin Adams <robin.adams.78@gmail.com>
2021-01-31 01:58:44 +0100

    Document IScreenReader

--- src/model/rogue/IScreenReader.scala
+++ src/model/rogue/IScreenReader.scala
@@ -1,6 +1,6 @@
 package model.rogue
 
+/** An observer that is notified whenever the screen displayed by Rogue changes */
 trait IScreenReader extends IScreenObserver {
   def readScreen(): Option[Screen]
-
 }
